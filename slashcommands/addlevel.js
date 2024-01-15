651f1f3b3f78b93191647d4678946de9136ecfde
Mendo6472 <mendo6472@gmail.com>
2024-01-15 12:47:48 -0500

    Added more checks to addlevel command
    Now checks if the role is managed and if the role is higher than the bot's highest role

--- slashcommands/addlevel.js
+++ slashcommands/addlevel.js
@@ -23,6 +23,7 @@ module.exports = {
         //Defering reply to avoid timeout
         await interaction.deferReply()
         
+        //Get the level and the role from the interaction
         var level = interaction.options.getInteger("level")
         var role = interaction.options.getRole("role")
 
@@ -32,12 +33,22 @@ module.exports = {
         var levels = JSON.parse(levelsFile)
         //Check if the level is already in the rank system
         if(levels[level] != null){
-            //If it is, alert them
             await interaction.editReply({ content: "That level is already in the rank system!", ephemeral: true });
+            return
+        }
+        //Check if the role is managed (A bot's role or a weebhook's role)
+        if(role.managed){
+            await interaction.editReply({ content: "You can't add this role to the rank system!", ephemeral: true });
+            return
+        }
+        //Check if the role is higher than the bot's highest role
+        if(role.rawPosition >= interaction.guild.members.me.roles.highest.position){
+            await interaction.editReply({ content: "I can't add this role to the rank system!", ephemeral: true });
             return
         }
         //Add the level to the rank system
         levels[level] = role.id
+        console.log(role)
         //Save the file
         fs.writeFileSync("./db/levels.json", JSON.stringify(levels))
         //Confirm the level
