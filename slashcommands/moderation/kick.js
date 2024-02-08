86db4dbc078d02b9765ba0fbe93c4a9fd305373e
Mendo6472 <mendo6472@gmail.com>
2024-02-08 08:38:31 -0500

    Added ability to dm kick reason and modified ephemeral messages

--- slashcommands/moderation/kick.js
+++ slashcommands/moderation/kick.js
@@ -19,11 +19,12 @@ module.exports = {
         .setDescription("Reason to kick the user")
         .setRequired(false)
         .setMinLength(1)
-        .setMaxLength(255)),
+        .setMaxLength(255))
+    .addBooleanOption(option =>
+        option.setName("dm")
+        .setDescription("Send a DM to the user with the reason")
+        .setRequired(false)),
     async run(client, interaction) {
-        // Defering reply to avoid timeout
-        await interaction.deferReply({ ephemeral: true });
-
         // Creating an embed to send errors
         const errorEmbed = new Discord.EmbedBuilder()
             .setTitle("❌ | Error")
@@ -36,48 +37,59 @@ module.exports = {
         // If the user is trying to kick themselves, alert them
         if (interaction.user.id === target.id) {
             errorEmbed.setDescription("You can't kick yourself!");
-            return await interaction.editReply({ embeds: [errorEmbed], ephemeral: true });
+            return await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
         }
 
         // If the user is not kickable, alert them
         if (!interaction.guild.members.cache.get(target.id).kickable) {
             errorEmbed.setDescription("That user is not kickable!");
-            return await interaction.editReply({ embeds: [errorEmbed], ephemeral: true });
+            return await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
         }
 
         // Si el usuario que desea kickear tiene un rol superior al del usuario que ejecuto el comando.
         if (member.roles.highest.position >= interaction.member.roles.highest.position) {
             errorEmbed.setDescription("I can't kick this user because it has the same role or a superior role to yours.");
-            return await interaction.editReply({ embeds: [errorEmbed], ephermal: true });
+            return await interaction.reply({ embeds: [errorEmbed], ephermal: true });
         }
   
         // Si el usuario que desea kickear tiene un rol superior al del bot.
         if (member.roles.highest.position >= interaction.guild.members.me.roles.highest.position) {
             errorEmbed.setDescription("I can't kick this user because it has the same role or a superior role to mine.");
-            return await interaction.editReply({ embeds: [errorEmbed], ephermal: true });
+            return await interaction.reply({ embeds: [errorEmbed], ephermal: true });
         }
 
         // Si el usuario que desea kickear sea el propio bot.
         if (target.id === client.user.id) {
-            errorEmbed.setDescription("No me puedo kickear a mi mismo.")
-            return await interaction.editReply({ embeds: [errorEmbed], ephermal: true })
+            errorEmbed.setDescription("I can't kick myself!")
+            return await interaction.reply({ embeds: [errorEmbed], ephermal: true })
         }
 
         try {
             await member.kick({ reason })
+            //Send a DM to the user with the reason
+            if (interaction.options.getBoolean("dm")) {
+                const dmEmbed = new Discord.EmbedBuilder()
+                    .setTitle("👞 | Kicked")
+                    .setDescription(`You have been kicked from the server **${interaction.guild.name}**.`)
+                    .addFields({ name: "▸ 📄 Reason", value: `>>> ${reason}` })
+                    .setColor(client.color)
+                    .setTimestamp()
+                await target.send({ embeds: [dmEmbed] })
+            }
+            //Send a reply to the interaction
             const kickEmbed = new Discord.EmbedBuilder()
               .setTitle("👞 | Kick")
               .setThumbnail(`https://cdn.discordapp.com/avatars/${target.id}/${target.avatar}`)
               .setDescription(`An user has been kicked in this server.`)
               .addFields({ name: `▸ 👤 User`, value: `>>> **Username:** ${target.username}\n**ID:** ${target.id}` })
-              .addFields({ name: `▸ 📄 Razón`, value: `>>> \`${reason}\`` })
+              .addFields({ name: `▸ 📄 Reason`, value: `>>> ${reason}` })
               .setColor(client.color)
               .setTimestamp()
       
-            await interaction.editReply({ embeds: [kickEmbed] })
+            await interaction.reply({ embeds: [kickEmbed], ephemeral: false})
           } catch (error) {
             errorEmbed.setDescription("An error has ocurred when trying to kick this user")
-            await interaction.editReply({ embeds:[errorEmbed], ephemeral: true })
+            await interaction.reply({ embeds:[errorEmbed], ephemeral: true})
             console.error(error)
           }
     }
