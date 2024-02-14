45af89ab814fdf91c16e85d3030f2861de5bfd81
Mendo6472 <mendo6472@gmail.com>
2024-02-14 12:54:33 -0500

    Fixed bug stopping checking streamers when one is not streaming

--- src/streamChecker.js
+++ src/streamChecker.js
@@ -15,11 +15,12 @@ const checkStreamersStatus = async (client) => {
     for (const streamer in streamers) {
         //Check if the streamer is live
         const streamId = await checkStreamerStatus(streamers[streamer], streamer, client)
-        if(streamId == false) return;
-        streamers[streamer].lastStreamId = streamId;
-        fs.writeFileSync("./db/streamAlerts.json", JSON.stringify(streamers, null, 4)),(err) => {
-            if(err){
-                console.error(err);
+        if(streamId != false){
+            streamers[streamer].lastStreamId = streamId;
+            fs.writeFileSync("./db/streamAlerts.json", JSON.stringify(streamers, null, 4)),(err) => {
+                if(err){
+                    console.error(err);
+                }
             }
         }
     }
